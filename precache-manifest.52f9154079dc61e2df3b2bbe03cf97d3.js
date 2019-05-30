d61d8dd4eb3e2d816d07d87dbcccedfbb199770e
17robots <mdray@ameritech.net>
2019-05-30 22:23:29 +0000

    Updates

--- precache-manifest.52f9154079dc61e2df3b2bbe03cf97d3.js
+++ precache-manifest.52f9154079dc61e2df3b2bbe03cf97d3.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3a7051caf9df4cd3aa65306b9f708548",
+    "revision": "741e82110dd42ac34748e1710e283ec1",
     "url": "/index.html"
   },
   {
-    "revision": "4b623e8593c9ac990379",
-    "url": "/static/css/main.2cce8147.chunk.css"
+    "revision": "fd8ad1ae94fe19c2c496",
+    "url": "/static/css/main.19393e92.chunk.css"
   },
   {
     "revision": "2e0129063922e3bb197a",
     "url": "/static/js/2.b41502e9.chunk.js"
   },
   {
-    "revision": "4b623e8593c9ac990379",
-    "url": "/static/js/main.28647029.chunk.js"
+    "revision": "fd8ad1ae94fe19c2c496",
+    "url": "/static/js/main.bc5ba69e.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
